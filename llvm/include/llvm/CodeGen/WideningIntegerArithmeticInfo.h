09ef4bc3b5917b49b091da10a89d2a62e25849ad
Panagiotis K <karouzakisp@gmail.com>
2023-07-26 07:13:59 +0000

    [LLVM][feature] Added classes to map integer arithmetic expressions

--- llvm/include/llvm/CodeGen/WideningIntegerArithmeticInfo.h
+++ llvm/include/llvm/CodeGen/WideningIntegerArithmeticInfo.h
@@ -10,9 +10,9 @@ namespace llvm {
 class Instruction;
 
 enum IntegerFillType{
-  sign_bits = 0,
-  zeroes,
-  anything 
+  SIGN = 0,
+  ZEROS,
+  ANYTHING,
 };
 
 
@@ -20,90 +20,354 @@ enum IntegerFillType{
 // of WideningIntegerArithmetic class requires.
 class WideningIntegerSolutionInfo{
 public:
- 
-  ~WideningIntegerSolutionInfo();
-  WideningIntegerSolutionInfo() = default; 
-  WideningIntegerSolutionInfo(Instruction *Instr, IntegerFillType fillType_,
-                      unsigned char width_)
-    : IntegerInstr(Instr), FillType(fillType_), Width(width_){} 
+
+  WideningIntegerSolutionInfo(){}  
+  WideningIntegerSolutionInfo(unsigned char Opcode_, IntegerFillType FillType_,
+            unsigned char Width_, unsigned char UpdatedWidth_, 
+            short int Cost_): 
+  Width(Width_), FillType(FillType_), UpdatedWidth(UpdatedWidth_),
+  Cost(Cost_), Opcode(Opcode_) {} 
   
+  virtual bool operator==(const WideningIntegerSolutionInfo &a) = 0; 
 
-private:
-  
 
-  // We care only about integer arithmetic instructions
-  Instruction       *IntegerInstr=nullptr;
-
-  // The updated instruction if we add an extension
-  Instruction       *UpdatedInstr=nullptr;
-
-  // The cost of the extension 
-  short int         ExtensionCost=0;
-
-  // Fill type for the upper bits of the expression
-  // can be zero, one or garbage
-  IntegerFillType   FillType=anything;
-
-  // The updated width of the instruction's destination operand
-  unsigned char     UpdatedWidth=0;
-
-  // The current width of the instruction's destination operand
-  unsigned char     Width=0;
-
-public:
-  Instruction *getInstruction(void){
-    return IntegerInstr;
+  unsigned getOpcode(void) const {
+    return Opcode;
   }
-  void setIntegerInstruction(Instruction *IntegerInstr_){
-    IntegerInstr = IntegerInstr_;
+  void setOpcode(unsigned Opcode_){
+    Opcode = Opcode_;
   }  
 
-  Instruction *getUpdatedInstruction(void){
-    return UpdatedInstr;
+  unsigned getUpdatedOpcode(void) const {
+    return UpdatedOpcode;
   }
-  void setUpdatedInstruction(Instruction *UpdatedInstr_){
-    UpdatedInstr = UpdatedInstr_;
-  }
+  void setUpdatedOpcode(unsigned UpdatedOpcode_){
+    UpdatedOpcode = UpdatedOpcode_;
+  }  
   
-  short int getExtensionCost(void){
-    return ExtensionCost;
+  short int getCost(void) const {
+    return Cost;
   }
-  void setExtensionCost(short int ExtensionCost_){
-    ExtensionCost = ExtensionCost_;
+  void setCost(short int Cost_){
+    Cost = Cost_;
   }
-  IntegerFillType getIntegerFillType(void){
+  IntegerFillType getFillType(void) const {
     return FillType;
   }
-  void setIntegerFillType(IntegerFillType FillType_){
+  void setFillType(IntegerFillType FillType_){
     FillType = FillType_; 
   }
 
-  unsigned char getWidth(void){
+  unsigned char getWidth(void) const {
     return Width;
   }
   void setWidth(unsigned char Width_){
     Width = Width_;
   }
   
-  unsigned char getUpdatedWidth(void){
+  unsigned char getUpdatedWidth(void) const {
     return UpdatedWidth;
   }
 
   void setUpdatedWidth(unsigned char UpdatedWidth_){
     UpdatedWidth = UpdatedWidth_;
   }
-  bool operator==(const WideningIntegerSolutionInfo& a){
-    return  this->Width == a.Width && 
-            this->FillType == a.FillType &&
-            this->ExtensionCost == a.ExtensionCost &&
-            this->UpdatedInstr == a.UpdatedInstr &&
-            this->IntegerInstr == a.IntegerInstr;
-  }
   
 
+protected:
+  
+
+  // need reference to other solutions
+  // left and right 
+
+  // We care only about integer arithmetic instructions
+  unsigned       Opcode;
+  
+  // Fill type for the upper bits of the expression
+  // can be zero, one or garbage
+  IntegerFillType   FillType;
+
+
+
+  // The current width of the instruction's destination operand
+  unsigned char     Width;
+
+  // The updated width of the instruction's destination operand
+  unsigned char     UpdatedWidth;
+  
+  // The cost of the solution
+  short int         Cost;
+
+  
+  // The updated instruction if we add an extension
+  unsigned       UpdatedOpcode;
   
 
 }; // WideningIntegerSolutionInfo
+
+
+class WIA_BINOP : public WideningIntegerSolutionInfo
+{
+  public:
+  WIA_BINOP() {}
+  ~WIA_BINOP() {}
+  WIA_BINOP(unsigned char Opcode_, IntegerFillType FillType_,
+            unsigned char Width_, unsigned char UpdatedWidth_, 
+            short int Cost_): 
+      WideningIntegerSolutionInfo::WideningIntegerSolutionInfo(
+        Opcode_, FillType_, Width_, UpdatedWidth_, Cost_) {}
+
+  
+  virtual bool operator==(const WideningIntegerSolutionInfo& a) override {
+    return  Width == a.getWidth() && 
+            FillType == a.getFillType() &&
+            Cost == a.getCost() &&
+            UpdatedOpcode == a.getUpdatedOpcode() &&
+            Opcode == a.getOpcode();
+  }
+};
+
+
+class WIA_FILL : public WideningIntegerSolutionInfo
+{
+  public:
+  WIA_FILL() {}
+  ~WIA_FILL() {}
+  WIA_FILL(unsigned char Opcode_, IntegerFillType FillType_,
+            unsigned char Width_, unsigned char UpdatedWidth_, 
+            short int Cost_): 
+      WideningIntegerSolutionInfo::WideningIntegerSolutionInfo(
+        Opcode_, FillType_, Width_, UpdatedWidth_, Cost_) {}
+
+  
+  virtual bool operator==(const WideningIntegerSolutionInfo& a) override {
+    return  Width == a.getWidth() && 
+            FillType == a.getFillType() &&
+            Cost == a.getCost() &&
+            UpdatedOpcode == a.getUpdatedOpcode() &&
+            Opcode == a.getOpcode();
+  }
+};
+
+class WIA_WIDEN_ZERO : public WideningIntegerSolutionInfo
+{
+  public:
+  WIA_WIDEN_ZERO() {}
+  ~WIA_WIDEN_ZERO() {}
+  WIA_WIDEN_ZERO(unsigned char Opcode_, IntegerFillType FillType_,
+            unsigned char Width_, unsigned char UpdatedWidth_, 
+            short int Cost_): 
+      WideningIntegerSolutionInfo::WideningIntegerSolutionInfo(
+        Opcode_, FillType_, Width_, UpdatedWidth_, Cost_) {}
+
+  
+  virtual bool operator==(const WideningIntegerSolutionInfo& a) override {
+    return  Width == a.getWidth() && 
+            FillType == a.getFillType() &&
+            Cost == a.getCost() &&
+            UpdatedOpcode == a.getUpdatedOpcode() &&
+            Opcode == a.getOpcode();
+  }
+};
+
+class WIA_WIDEN_SIGN : public WideningIntegerSolutionInfo
+{
+  public:
+  WIA_WIDEN_SIGN() {}
+  ~WIA_WIDEN_SIGN() {}
+  WIA_WIDEN_SIGN(unsigned char Opcode_, IntegerFillType FillType_,
+            unsigned char Width_, unsigned char UpdatedWidth_, 
+            short int Cost_): 
+      WideningIntegerSolutionInfo::WideningIntegerSolutionInfo(
+        Opcode_, FillType_, Width_, UpdatedWidth_, Cost_) {}
+
+  
+  virtual bool operator==(const WideningIntegerSolutionInfo& a) override {
+    return  Width == a.getWidth() && 
+            FillType == a.getFillType() &&
+            Cost == a.getCost() &&
+            UpdatedOpcode == a.getUpdatedOpcode() &&
+            Opcode == a.getOpcode();
+  }
+};
+
+
+class WIA_NARROW : public WideningIntegerSolutionInfo
+{
+  public:
+  WIA_NARROW() {}
+  ~WIA_NARROW() {}
+  WIA_NARROW(unsigned char Opcode_, IntegerFillType FillType_,
+            unsigned char Width_, unsigned char UpdatedWidth_, 
+            short int Cost_): 
+      WideningIntegerSolutionInfo::WideningIntegerSolutionInfo(
+        Opcode_, FillType_, Width_, UpdatedWidth_, Cost_) {}
+
+  
+  virtual bool operator==(const WideningIntegerSolutionInfo& a) override {
+    return  Width == a.getWidth() && 
+            FillType == a.getFillType() &&
+            Cost == a.getCost() &&
+            UpdatedOpcode == a.getUpdatedOpcode() &&
+            Opcode == a.getOpcode();
+  }
+};
+
+
+class WIA_DROP_EXT : public WideningIntegerSolutionInfo
+{
+  public:
+  WIA_DROP_EXT() {}
+  ~WIA_DROP_EXT() {}
+  WIA_DROP_EXT(unsigned char Opcode_, IntegerFillType FillType_,
+            unsigned char Width_, unsigned char UpdatedWidth_, 
+            short int Cost_): 
+      WideningIntegerSolutionInfo::WideningIntegerSolutionInfo(
+        Opcode_, FillType_, Width_, UpdatedWidth_, Cost_) {}
+
+  
+  virtual bool operator==(const WideningIntegerSolutionInfo& a) override {
+    return  Width == a.getWidth() && 
+            FillType == a.getFillType() &&
+            Cost == a.getCost() &&
+            UpdatedOpcode == a.getUpdatedOpcode() &&
+            Opcode == a.getOpcode();
+  }
+};
+
+
+class WIA_DROP_LOCOPY : public WideningIntegerSolutionInfo
+{
+  public:
+  WIA_DROP_LOCOPY() {}
+  ~WIA_DROP_LOCOPY() {}
+  WIA_DROP_LOCOPY(unsigned char Opcode_, IntegerFillType FillType_,
+            unsigned char Width_, unsigned char UpdatedWidth_, 
+            short int Cost_): 
+      WideningIntegerSolutionInfo::WideningIntegerSolutionInfo(
+        Opcode_, FillType_, Width_, UpdatedWidth_, Cost_) {}
+
+  
+  virtual bool operator==(const WideningIntegerSolutionInfo& a) override {
+    return  Width == a.getWidth() && 
+            FillType == a.getFillType() &&
+            Cost == a.getCost() &&
+            UpdatedOpcode == a.getUpdatedOpcode() &&
+            Opcode == a.getOpcode();
+  }
+};
+
+
+class WIA_DROP_LOIGNORE : public WideningIntegerSolutionInfo
+{
+  public:
+  WIA_DROP_LOIGNORE() {}
+  ~WIA_DROP_LOIGNORE() {}
+  WIA_DROP_LOIGNORE(unsigned char Opcode_, IntegerFillType FillType_,
+            unsigned char Width_, unsigned char UpdatedWidth_, 
+            short int Cost_): 
+      WideningIntegerSolutionInfo::WideningIntegerSolutionInfo(
+        Opcode_, FillType_, Width_, UpdatedWidth_, Cost_) {}
+
+  
+  virtual bool operator==(const WideningIntegerSolutionInfo& a) override {
+    return  Width == a.getWidth() && 
+            FillType == a.getFillType() &&
+            Cost == a.getCost() &&
+            UpdatedOpcode == a.getUpdatedOpcode() &&
+            Opcode == a.getOpcode();
+  }
+};
+
+
+class WIA_EXTLO : public WideningIntegerSolutionInfo
+{
+  public:
+  WIA_EXTLO() {}
+  ~WIA_EXTLO() {}
+  WIA_EXTLO(unsigned char Opcode_, IntegerFillType FillType_,
+            unsigned char Width_, unsigned char UpdatedWidth_, 
+            short int Cost_): 
+      WideningIntegerSolutionInfo::WideningIntegerSolutionInfo(
+        Opcode_, FillType_, Width_, UpdatedWidth_, Cost_) {}
+
+  
+  virtual bool operator==(const WideningIntegerSolutionInfo& a) override {
+    return  Width == a.getWidth() && 
+            FillType == a.getFillType() &&
+            Cost == a.getCost() &&
+            UpdatedOpcode == a.getUpdatedOpcode() &&
+            Opcode == a.getOpcode();
+  }
+};
+
+
+class WIA_SUBSUME_FILL : public WideningIntegerSolutionInfo
+{
+  public:
+  WIA_SUBSUME_FILL() {}
+  ~WIA_SUBSUME_FILL() {}
+  WIA_SUBSUME_FILL(unsigned char Opcode_, IntegerFillType FillType_,
+            unsigned char Width_, unsigned char UpdatedWidth_, 
+            short int Cost_): 
+      WideningIntegerSolutionInfo::WideningIntegerSolutionInfo(
+        Opcode_, FillType_, Width_, UpdatedWidth_, Cost_) {}
+
+  
+  virtual bool operator==(const WideningIntegerSolutionInfo& a) override {
+    return  Width == a.getWidth() && 
+            FillType == a.getFillType() &&
+            Cost == a.getCost() &&
+            UpdatedOpcode == a.getUpdatedOpcode() &&
+            Opcode == a.getOpcode();
+  }
+};
+
+
+class WIA_SUBSUME_INDEX : public WideningIntegerSolutionInfo
+{
+  public:
+  WIA_SUBSUME_INDEX() {}
+  ~WIA_SUBSUME_INDEX() {}
+  WIA_SUBSUME_INDEX(unsigned char Opcode_, IntegerFillType FillType_,
+            unsigned char Width_, unsigned char UpdatedWidth_, 
+            short int Cost_): 
+      WideningIntegerSolutionInfo::WideningIntegerSolutionInfo(
+        Opcode_, FillType_, Width_, UpdatedWidth_, Cost_) {}
+
+  
+  virtual bool operator==(const WideningIntegerSolutionInfo& a) override {
+    return  Width == a.getWidth() && 
+            FillType == a.getFillType() &&
+            Cost == a.getCost() &&
+            UpdatedOpcode == a.getUpdatedOpcode() &&
+            Opcode == a.getOpcode();
+  }
+};
+
+
+class WIA_NATURAL : public WideningIntegerSolutionInfo
+{
+  public:
+  WIA_NATURAL() {}
+  ~WIA_NATURAL() {}
+  WIA_NATURAL(unsigned char Opcode_, IntegerFillType FillType_,
+            unsigned char Width_, unsigned char UpdatedWidth_, 
+            short int Cost_): 
+      WideningIntegerSolutionInfo::WideningIntegerSolutionInfo(
+        Opcode_, FillType_, Width_, UpdatedWidth_, Cost_) {}
+
+  
+  virtual bool operator==(const WideningIntegerSolutionInfo& a) override {
+    return  Width == a.getWidth() && 
+            FillType == a.getFillType() &&
+            Cost == a.getCost() &&
+            UpdatedOpcode == a.getUpdatedOpcode() &&
+            Opcode == a.getOpcode();
+  }
+};
+
+
 
 
 
--- llvm/lib/CodeGen/SelectionDAG/WideningIntegerArithmetic.cpp
+++ llvm/lib/CodeGen/SelectionDAG/WideningIntegerArithmetic.cpp
@@ -8,14 +8,10 @@
 #include "llvm/CodeGen/TargetLowering.h"
 #include "llvm/CodeGen/SelectionDAGNodes.h"
 #include "llvm/CodeGen/WideningIntegerArithmeticInfo.h"
+#include "llvm/ADT/SmallSet.h"
 
 using namespace llvm;
 
-enum fillType{
-  ZEROS = 0,
-  SIGN_BIT = 1,
-  GARBAGE = 2
-};
 
 class WideningIntegerArithmetic {
   SelectionDAG &DAG;
@@ -29,22 +25,63 @@ class WideningIntegerArithmetic {
     void solve();
 
   private:
-    enum fillType FillType;
+    enum IntegerFillType ExtensionChoice;   
+ 
+    using isSolvedMap = DenseMap<int, bool>;
+    // checks whether a SDNode in the DAG is visited and solved` 
+    isSolvedMap solvedNodes;
     
-    using SolutionMap = DenseMap<int, WideningIntegerSolutionInfo *>; 
-    SolutionMap Solutions;
+    using SolutionSet = SmallVector<WideningIntegerSolutionInfo *>;
     
-    using AvailableSolutionsMap = DenseMap<int, 
-                    SmallVector<WideningIntegerSolutionInfo *>>;  
+    using AvailableSolutionsMap = DenseMap<int, SolutionSet>;
+    // Holds all the available solutions 
     AvailableSolutionsMap AvailableSolutions;
+
+    using FillTypeSet = SmallSet<IntegerFillType, 4>;
  
     SDValue visit_widening(SDNode *Node);
     SDValue visitInstruction(SDNode *Node);
     SDValue visitANY_EXT_OR_TRUNC(SDNode *Node);
     SDValue visitXOR(SDNode *Node);
+    SDValue visitADD(SDNode *Node);
+    SDValue visitADDO(SDNode *Node);
   
     void setFillType(EVT SrcVt, EVT DstVT);  
-    bool isInteger(SDNode *Node); 
+    bool isInteger(SDNode *Node);
+    bool isSolved(SDNode *Node);
+    void combineSolutions(int NodeId, unsigned Opcode, SolutionSet leftSols,
+SolutionSet rightSols, FillTypeSet operandFillTypes);
+    bool canCombineSolutions(WideningIntegerSolutionInfo *leftSol, 
+                             WideningIntegerSolutionInfo *rightSol, 
+                             FillTypeSet OperandFillTypes);
+    
+    WideningIntegerSolutionInfo *visitBINOP(
+                          WideningIntegerSolutionInfo *LeftSol,
+                          WideningIntegerSolutionInfo *RightSol);
+    WideningIntegerSolutionInfo *visitFILL(
+                          WideningIntegerSolutionInfo *Sol);
+    WideningIntegerSolutionInfo *visitWIDEN(
+                          WideningIntegerSolutionInfo *Sol);
+    WideningIntegerSolutionInfo *visitWIDEN_GARBAGE(
+                          WideningIntegerSolutionInfo *Sol);
+    WideningIntegerSolutionInfo *visitNARROW(
+                          WideningIntegerSolutionInfo *Sol);
+    WideningIntegerSolutionInfo *visitDROP_EXT(
+                          WideningIntegerSolutionInfo *Sol);
+    WideningIntegerSolutionInfo *visitDROP_LO_COPY(
+                          WideningIntegerSolutionInfo *Sol);
+    WideningIntegerSolutionInfo *visitDROP_LO_IGNORE(
+                          WideningIntegerSolutionInfo *Sol);
+    WideningIntegerSolutionInfo *visitEXTLO(
+                          WideningIntegerSolutionInfo *LeftSol,
+                          WideningIntegerSolutionInfo *RightSol);
+    WideningIntegerSolutionInfo *visitSUBSUME_FILL(
+                          WideningIntegerSolutionInfo *Sol);
+    WideningIntegerSolutionInfo *visitSUBSUME_INDEX(
+                          WideningIntegerSolutionInfo *Sol);
+    WideningIntegerSolutionInfo *visitNATURAL(
+                          WideningIntegerSolutionInfo *Sol);
+     
 };
 
 SDValue WideningIntegerArithmetic::visitInstruction(SDNode *Node){
@@ -53,8 +90,10 @@ SDValue WideningIntegerArithmetic::visitInstruction(SDNode *Node){
     default: break;
     case ISD::SIGN_EXTEND:
     case ISD::ZERO_EXTEND:    
-    case ISD::TRUNCATE:       return visitANY_EXT_OR_TRUNC(Node );
-    case ISD::XOR:            return visitXOR(Node ); 
+    case ISD::TRUNCATE:       return visitANY_EXT_OR_TRUNC(Node);
+    case ISD::XOR:            return visitXOR(Node); 
+    case ISD::ADD:            return visitADD(Node);
+    case ISD::UADDO:          return visitADDO(Node);
   }
   return SDValue(Node, 0);
 }
@@ -62,12 +101,16 @@ SDValue WideningIntegerArithmetic::visitInstruction(SDNode *Node){
 
 SDValue WideningIntegerArithmetic::visit_widening(SDNode *Node){
   int NumOperands = Node->getNumOperands();
-  if(NumOperands == 0 ){
+  if(NumOperands == 0 ){ // TODO how to check leaf is probably correct need to verify 
+                         // TODO check if not solved 
+                        // 2 paths can lead to the same node
+                        // is already checked
     return visitInstruction(Node);
   }
   else {
     for (const SDValue &value : Node->op_values() ){
-      SDNode *OperandNode = value.getNode();
+        
+      SDNode *OperandNode = value.getNode(); // #TODO check value size is 1?
       SDValue N = visit_widening(OperandNode);
       return N;
     }
@@ -75,64 +118,165 @@ SDValue WideningIntegerArithmetic::visit_widening(SDNode *Node){
   return SDValue(Node, 0);
 }
 
+
+bool WideningIntegerArithmetic::canCombineSolutions(
+                                WideningIntegerSolutionInfo *leftSolution, 
+                                WideningIntegerSolutionInfo *rightSolution, 
+                                FillTypeSet operandFillTypes){
+  if(leftSolution->getFillType() != rightSolution->getFillType()){ 
+    return false;
+  }else if(!operandFillTypes.contains(leftSolution->getFillType())){
+    return false;  
+  }
+
+  return true; 
+  
+
+}
+
+void WideningIntegerArithmetic::combineSolutions(int NodeId, unsigned Opcode, SolutionSet LeftSols, 
+        SolutionSet RightSols, FillTypeSet OperandFillTypes){
+  for (auto leftSolution : LeftSols){
+    for(auto rightSolution : RightSols){
+      if(canCombineSolutions(leftSolution, rightSolution, OperandFillTypes)){
+        unsigned char Cost = leftSolution->getCost() + rightSolution->getCost();
+        unsigned char LeftWidth = leftSolution->getUpdatedWidth();
+        unsigned char RightWidth = rightSolution->getUpdatedWidth();
+        unsigned char NewWidth = LeftWidth > RightWidth ? LeftWidth : 
+                                                          RightWidth;
+        auto Sol = new WIA_BINOP(Opcode, 
+          leftSolution->getFillType(), NewWidth, NewWidth, Cost);
+
+        AvailableSolutions[NodeId].push_back(Sol); 
+      }    
+    }
+  }
+}
+
 SDValue WideningIntegerArithmetic::visitXOR(SDNode *Node ){
   SDValue N0 = Node->getOperand(0);
   SDValue N1 = Node->getOperand(1);
-  auto LeftSolution = Solutions.find((N0.getNode())->getNodeId());
-  auto RightSolution = Solutions.find((N1.getNode())->getNodeId());
-  assert(LeftSolution != Solutions.end() && "Xor Dag Child not solved yet"); 
-  assert(RightSolution != Solutions.end() && "Xor Dag Child not solved yet");
-  int left_cost = LeftSolution->second->getExtensionCost();
-  int right_cost = RightSolution->second->getExtensionCost();
-  
-  auto leftAvailableSolutions = 
-      AvailableSolutions.find((N0.getNode())->getNodeId());
-  auto rightAvailableSolutions = 
-      AvailableSolutions.find((N1.getNode())->getNodeId());
-  // add left and right to Node available Solutions 
-  
+  FillTypeSet OperandFillTypes;
+  OperandFillTypes.insert(SIGN);
+  OperandFillTypes.insert(ZEROS);
+  OperandFillTypes.insert(ANYTHING);
+
+  // get All the available solutions from nodes 
+  SolutionSet LeftSols = 
+      AvailableSolutions.find((N0.getNode())->getNodeId())->second;
+  SolutionSet  RightSols = 
+      AvailableSolutions.find((N1.getNode())->getNodeId())->second;
+  // add A function that combines the solution
+  combineSolutions(Node->getNodeId(), Node->getOpcode(),  LeftSols,
+                   RightSols,   OperandFillTypes);
+  // based on below 
+  // add with empty set
+  // based on opcode operand fillType for XOR
+  // can be either g, s, z
+  // TODO FOR MY operand what is the fill type? 
+  // g means GARBAGE , g means anything, we cannot trust the extended bits
+  // s means  that the fill type is signed bits 
+  // z means that the fill type is zero bits
+  // τ means means that fill type can be anything s, z, or g
+  // . left operand
+  // . {2 solutions: σ[32]:64--cost:4 , g[32]:64--cost:1}
+  // σ[32]:64 means . sign extend (no garbage) upper unused bits zeros or one.
+  // g[32]:64 means you cannot trust the high 32 bits
+  // g[16]:64 means you cannot trust the high 48 bits
+  // . right operand
+  // . {1 solutions: σ[32]:i64 cost :3 }
+  // . check legal values for XOR can
+  // xor legal types
+  // { s -> s x s }, {g-> g x g}, { z -> z x z } 
+  // We can  choose 
+  // Solution 1 s[32]:64 cost 4 + 3, Solution 2 g[32]:64 cost 3 needs sext or zext depends on the fill type of target so total 3 + 1 }
+  // User can decide what solution is appropriate
+  // for example print requires sext to use Solution 2 because upper  // upper bits
+  // {i32, i32, i32 } 
+  // can we Truncate? 
   return SDValue(Node, 0);
 }
+
 
 SDValue WideningIntegerArithmetic::visitANY_EXT_OR_TRUNC(SDNode *Node){
   SDValue N0 = Node->getOperand(0);
   SDValue N1 = Node->getOperand(1);
   
-  // TODO check this
+  // TODO check this do we know that the result is in index 0?
+  
   setFillType(N0->getValueType(0), N1->getValueType(0));
   
   // just drop the extension or truncation 
-  //we will measure later if this produces incorrect results
+  // we will measure later if this produces incorrect results
   return N0; 
   
 }
 
+SDValue WideningIntegerArithmetic::visitADD(SDNode *Node ){
+  SDValue N0 = Node->getOperand(0);
+  SDValue N1 = Node->getOperand(1);
+  bool IsSigned = (ISD::SADDO == Node->getOpcode());
+  FillTypeSet OperandFillTypes;
+  
+  OperandFillTypes.insert(ANYTHING);
+
+  // TODO check if it overflows based on ISD::ADD
+  // and if it does not for sure we can add SIGN AND ZEROS ?
+  if(!IsSigned){
+    if(DAG.computeOverflowKind(N0, N1 ) == SelectionDAG::OFK_Never){
+      OperandFillTypes.insert(SIGN);
+      OperandFillTypes.insert(ZEROS);
+    }
+  }
+    
+  // get All the available solutions from nodes 
+  SolutionSet LeftSols = 
+      AvailableSolutions.find((N0.getNode())->getNodeId())->second;
+  SolutionSet  RightSols = 
+      AvailableSolutions.find((N1.getNode())->getNodeId())->second;
+  
+  combineSolutions(Node->getNodeId(), Node->getOpcode(),  LeftSols,
+                   RightSols,   OperandFillTypes);
+   
+  return SDValue(Node, 0); 
+
+}
+
 void WideningIntegerArithmetic::setFillType(EVT SrcVT, EVT DstVT){
   if(TLI.isSExtCheaperThanZExt(SrcVT, DstVT)){
-    FillType = SIGN_BIT;
+    ExtensionChoice = SIGN;
   }else{
-    FillType = ZEROS;
+    ExtensionChoice = ZEROS;
   }
 }
 
 void WideningIntegerArithmetic::solve(){
-  
+ 
+  SDValue Root = DAG.getRoot();
+  setFillType(Root.getValueType(), Root.getValueType());
   
   for (SDNode &Node : DAG.allnodes()){
-      auto Solution = Solutions.find(Node.getNodeId());  
-      if(Solution == Solutions.end() && 
-                     isInteger(&Node) ){ // If not solved visit it
+      if(!isSolved(&Node)  && isInteger(&Node) ){ 
         visit_widening(&Node);  
       }
     
   }
 } 
 
-bool isInteger(SDNode *Node){
+bool WideningIntegerArithmetic::isInteger(SDNode *Node){
   return Node->getValueType(0).isInteger();
 }
 
-
+// Checks whether a SDNode is visited 
+// so we don't visit and solve the same Node again
+bool WideningIntegerArithmetic::isSolved(SDNode *Node){
+  auto isVisited = solvedNodes.find(Node->getNodeId());
+  if(isVisited == solvedNodes.end())
+    return false;
+  else
+    return true; 
+  return true;
+}
 
 
 
