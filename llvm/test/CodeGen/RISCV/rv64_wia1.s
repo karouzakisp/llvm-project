256c46de33a42d92720ac26e8ba64420d4f360ca
Panagiotis K <karouzakisp@gmail.com>
2023-09-22 20:37:08 +0000

    Updated fillTypeWidth

---
+++ llvm/test/CodeGen/RISCV/rv64_wia1.s
@@ -0,0 +1,21 @@
+	.text
+	.attribute	4, 16
+	.attribute	5, "rv64i2p0"
+	.file	"rv64_wia1.ll"
+	.globl	test_load_and_cmp               # -- Begin function test_load_and_cmp
+	.p2align	2
+	.type	test_load_and_cmp,@function
+test_load_and_cmp:                      # @test_load_and_cmp
+# %bb.0:
+	addiw	a2, a2, 17
+	slli	a2, a2, 48
+	srai	a2, a2, 48
+	xor	a0, a0, a2
+	sext.w	a0, a0
+	slli	a1, a0, 3
+	sub	a0, a1, a0
+	ret
+.Lfunc_end0:
+	.size	test_load_and_cmp, .Lfunc_end0-test_load_and_cmp
+                                        # -- End function
+	.section	".note.GNU-stack","",@progbits
