b098adc15c9a6ca4da5a3548a75e0cc78eafb158
Panagiotis K <karouzakisp@gmail.com>
2024-02-08 13:27:38 +0200

    small change on test file

--- llvm/test/CodeGen/RISCV/rv64_wia1.s
+++ llvm/test/CodeGen/RISCV/rv64_wia1.s
@@ -1,19 +1,15 @@
 	.text
-	.attribute	4, 16
-	.attribute	5, "rv64i2p0"
 	.file	"rv64_wia1.ll"
 	.globl	test_load_and_cmp               # -- Begin function test_load_and_cmp
-	.p2align	2
+	.p2align	4, 0x90
 	.type	test_load_and_cmp,@function
 test_load_and_cmp:                      # @test_load_and_cmp
 # %bb.0:
-	lui	a1, %hi(gd)
-	lh	a1, %lo(gd)(a1)
-	xor	a0, a0, a1
-	slli	a0, a0, 48
-	srai	a0, a0, 48
-	addi	a0, a0, 10
-	ret
+	movq	gd@GOTPCREL(%rip), %rax
+	xorw	(%rax), %di
+	movswl	%di, %eax
+	addl	$10, %eax
+	retq
 .Lfunc_end0:
 	.size	test_load_and_cmp, .Lfunc_end0-test_load_and_cmp
                                         # -- End function
