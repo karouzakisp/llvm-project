d36edd0dd441e13d6ea715499829c7f3ab60c039
PanagiotisK <karouzakispan@gmail.com>
2024-02-29 02:03:39 +0200

    removed OptExtensions from SelectionDAG

--- llvm/include/llvm/CodeGen/SelectionDAG.h
+++ llvm/include/llvm/CodeGen/SelectionDAG.h
@@ -575,10 +575,6 @@ public:
   void Combine(CombineLevel Level, AAResults *AA,
                CodeGenOpt::Level OptLevel);
 
-  // This iterates over all the nodes in the SelectionDAG after each Combine, and removes
-  // redudant sign and zero extension by doing analysis of the DAG.
-  // Note that floating point Nodes are not supported yet.
-  void OptExtensions(CodeGenOpt::Level OptLevel);
 
   /// This transforms the SelectionDAG into a SelectionDAG that
   /// only uses types natively supported by the target.
